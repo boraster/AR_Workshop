a927b60a2397597cd836fdcbef72e24660e1ddf9
Bora Ilkinonu <borailkinonu@gmail.com>
2023-02-02 16:24:17 +0300

    Update CartItems.cs

--- Assets/Scripts/CartItems.cs
+++ Assets/Scripts/CartItems.cs
@@ -10,23 +10,42 @@ public class CartItems : MonoBehaviour
     public Dictionary<int, float> priceList;
     private PriceTag priceTag;
 
-    private void Start()
-    {
 
-        StartCoroutine(FillOutDictionaries());
+    private void Awake()
+    {
+        FillOutDictionaries();
     }
-
-    private IEnumerator FillOutDictionaries()
+    
+    private void FillOutDictionaries()
     {
-
-        yield return new WaitForSeconds(1.0f);
+    
+        
         itemsList = new Dictionary<int, GameObject>(prefabPicker.prefabList.Count);
         priceList = new Dictionary<int, float>(prefabPicker.prefabList.Count);
-
+    
         for (int i = 0; i < prefabPicker.prefabList.Count; i++)
         {
             itemsList.Add(i, prefabPicker.prefabList[i]);
             priceList.Add(i, prefabPicker.prefabList[i].GetComponent<PriceTag>().price);
         }
     }
+    // private void Start()
+    // {
+    //
+    //     StartCoroutine(FillOutDictionaries());
+    // }
+
+    // private IEnumerator FillOutDictionaries()
+    // {
+    //
+    //     yield return new WaitForSeconds(1.0f);
+    //     itemsList = new Dictionary<int, GameObject>(prefabPicker.prefabList.Count);
+    //     priceList = new Dictionary<int, float>(prefabPicker.prefabList.Count);
+    //
+    //     for (int i = 0; i < prefabPicker.prefabList.Count; i++)
+    //     {
+    //         itemsList.Add(i, prefabPicker.prefabList[i]);
+    //         priceList.Add(i, prefabPicker.prefabList[i].GetComponent<PriceTag>().price);
+    //     }
+    // }
 }
